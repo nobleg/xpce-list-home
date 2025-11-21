fdc95384896d3bd6a18109853d0f5140c894bb6c
Gary <gary@heronsbrook.net>
2025-11-21 15:56:38 +0000

    next c

--- main.pl
+++ main.pl
@@ -2,21 +2,34 @@
 :- use_module(library(process)).
 
 start :-
-    new(D, dialog('Home Directory Listing')),
+    % Determine initial directory (home)
+    expand_file_name('~', [HomeDir]),
+
+    new(D, dialog('Directory Listing')),
     send(D, size, size(800, 600)),
 
-    % Editor widget to show the directory listing
-    send(D, append, new(E, editor), below),
+    % Text field to hold the directory path
+    send(D, append, new(DirItem, text_item(directory, HomeDir))),
 
-    % Refresh button
-    send(D, append, button(refresh, message(@prolog, refresh, E))),
+    % Editor widget to display the listing
+    send(D, append, new(Editor, editor), below),
+
+    % Refresh button: calls refresh/2 in Prolog
+    send(D, append, button(refresh, message(@prolog, refresh, DirItem, Editor))),
 
     send(D, open),
-    refresh(E).
+    refresh(DirItem, Editor).
 
-refresh(Editor) :-
-    expand_file_name('~', [HomeDir]),
-    process_create(path(ls), [HomeDir], [stdout(pipe(Out))]),
+refresh(DirItem, Editor) :-
+    % Read directory string from the text field
+    get(DirItem, selection, Dir0),
+    % Expand ~ etc to a real path
+    expand_file_name(Dir0, [Dir]),
+
+    % Run ls on that directory and capture output
+    process_create(path(ls), [Dir], [stdout(pipe(Out))]),
     read_string(Out, _, Output),
     close(Out),
+
+    % Update the editor with the new listing
     send(Editor, contents, Output).
